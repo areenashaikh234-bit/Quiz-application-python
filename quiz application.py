152a0f1d9a5d892ec8fc7cbc386a28b6e38c2876
areenashaikh234-bit <areenashaikh234@gmail.com>
2026-06-19 13:33:35 +0530

    Add files via upload

---
+++ quiz application.py
@@ -0,0 +1,17 @@
+questions ={
+    "what is the capital of India?": "Delhi",
+    "Which language is used for python development?": "Python",
+    "How many days are there in a week?": "7"
+    }
+score=0
+for question, answer in questions.items():
+    user_answer = input(question+"")
+
+    if user_answer.lower() == answer.lower():
+        print("correct")
+        score += 1
+    else:
+        print("wrong!")
+
+print("\nQuiz completed!")
+print("your score:", score, "/", len(questions))
